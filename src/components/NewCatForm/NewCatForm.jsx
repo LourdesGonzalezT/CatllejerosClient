ffed46a98fd728adc4196f933778210fc4fe9d11
LourdesGonzalezT <LourdesGonzalezT@github.com>
2023-12-13 20:32:21 +0100

    form in process

--- src/components/NewCatForm/NewCatForm.jsx
+++ src/components/NewCatForm/NewCatForm.jsx
@@ -1,40 +1,67 @@
+import { useState } from 'react';
 import { Button, Form, Row, Col } from 'react-bootstrap';
 
 const NewCatForm = () => {
+    const [catData, setCatData] = useState({
+        name: '',
+        age: '',
+        sex: '',
+        aboutCat: '',
+        image1: '',
+        image2: ''
+    })
+
     return (
         <Form>
             <Form.Group className="mb-3" controlId="name">
                 <Form.Label>Nombre del gatete</Form.Label>
-                <Form.Control type="text" />
+                <Form.Control type="text" value={catData.name} />
             </Form.Group>
-
             <Row>
-                <Form.Group className="mb-3" controlId="name">
-                    <Form.Label>Nombre del gatete</Form.Label>
-                    <Form.Control type="text" />
-                </Form.Group>
-
                 <Col>
-                    <Form.Group className="mb-3" controlId="date">
-                        <Form.Label>Fecha</Form.Label>
-                        <Form.Control type="text" />
+                    <Form.Group className="mb-3" controlId="age">
+                        <Form.Label>Edad del gatete</Form.Label>
+                        <Form.Control type="text" value={catData.age} />
+                    </Form.Group>
+                </Col>
+                <Col>
+                    <Form.Group className="mb-3" controlId="sex">
+                        <Form.Label>Género:</Form.Label>
+                        <div>
+                            <Form.Check
+                                type="radio"
+                                label="Macho"
+                                name="sex"
+                                id="male"
+                                value={catData.sex}
+                            />
+                            <Form.Check
+                                type="radio"
+                                label="Hembra"
+                                name="sex"
+                                id="female"
+                                value={catData.sex}
+                            />
+                        </div>
                     </Form.Group>
                 </Col>
             </Row>
-            <Form.Group className="mb-3" controlId="name">
-                <Form.Label>Nombre del gatete</Form.Label>
-                <Form.Control type="text" />
+
+            <Form.Group className="mb-3" controlId="aboutCat">
+                <Form.Label>Información sobre el gatete</Form.Label>
+                <Form.Control as="textarea" style={{ height: '100px' }} maxLength={1000} value={catData.aboutCat} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="name">
-                <Form.Label>Nombre del gatete</Form.Label>
-                <Form.Control type="text" />
+            <Form.Group controlId="image1">
+                <Form.Label>Imagen 1 del gatete</Form.Label>
+                <Form.Control type="file" accept="image/*" value={catData.image1} />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="observations">
-                <Form.Label>Observaciones</Form.Label>
-                <Form.Control type="text" />
+
+            <Form.Group controlId="image2">
+                <Form.Label>Imagen 2 del gatete</Form.Label>
+                <Form.Control type="file" accept="image/*" value={catData.image2} />
             </Form.Group>
             <div className='d-grid'>
-                <Button variant="dark" type="submit">Crear Festival</Button>
+                <Button variant="dark" type="submit">Registrar gatete</Button>
             </div>
         </Form>
     )
